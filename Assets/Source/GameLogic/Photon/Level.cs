6403541b69ef536684d0fcef3b628334a11d9a62
daniilkalmykov <daniilk24960@gmail.com>
2023-06-05 13:20:35 +0600

    add disabling player while waiting

--- Assets/Source/GameLogic/Photon/Level.cs
+++ Assets/Source/GameLogic/Photon/Level.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using Photon.Pun;
+using UI.Buttons;
 using UnityEngine;
 
 namespace GameLogic.Photon
@@ -9,10 +10,15 @@ namespace GameLogic.Photon
         [SerializeField] private PlayerSpawner _playerSpawner;
         [SerializeField] private float _delayToStartGame;
         [SerializeField] private CoinsSpawner _coinsSpawner;
+        [SerializeField] private Joystick _joystick;
+        [SerializeField] private ShootingButton _shootingButton;
         
         private void Start()
         {
             _playerSpawner.SpawnPlayer();
+
+            _joystick.enabled = false;
+            _shootingButton.enabled = false;
 
             if (PhotonNetwork.CurrentRoom.PlayerCount == PhotonNetwork.CurrentRoom.MaxPlayers)
                 StartCoroutine(WaitTimeToStartGame());
@@ -26,8 +32,10 @@ namespace GameLogic.Photon
 
         private void StartGame()
         {
-            print("start");
             _coinsSpawner.Spawn();
+            
+            _joystick.enabled = true;
+            _shootingButton.enabled = true;
         }
 
         private IEnumerator WaitTimeToStartGame()
